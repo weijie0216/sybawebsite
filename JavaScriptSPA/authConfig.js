46c7078fea6546e209126372e8540740dcd36a41
Yap Wei Jie <weijieyap16@gmail.com>
2022-04-14 17:13:38 +0800

    Updated Redirect URL

--- JavaScriptSPA/authConfig.js
+++ JavaScriptSPA/authConfig.js
@@ -6,7 +6,7 @@ const msalConfig = {
   auth: {
     clientId: "3c7ac775-e5d7-4e16-a31c-2ee8abd5175a",
     authority: "https://login.microsoftonline.com/db992bae-4cb3-4086-8c91-55255b0c39fe",
-    redirectUri: "https://sybawebsite.azurewebsites.net:3000",
+    redirectUri: "https://sybawebsite.azurewebsites.net",
   },
   cache: {
     cacheLocation: "sessionStorage", // This configures where your cache will be stored
