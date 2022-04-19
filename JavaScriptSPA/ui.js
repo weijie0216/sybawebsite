c0266fe42e66fd07b6e38a3fffde25349602c651
Yap Wei Jie <weijieyap16@gmail.com>
2022-04-19 15:54:06 +0800

    main

--- JavaScriptSPA/ui.js
+++ JavaScriptSPA/ui.js
@@ -7,12 +7,10 @@ const mailButton = document.getElementById("readMail");
 const profileButton = document.getElementById("seeProfile");
 const profileDiv = document.getElementById("profile-div");
 
-
 function showWelcomeMessage(account) {
-
     // Reconfiguring DOM elements
     cardDiv.classList.remove('d-none');
-    welcomeDiv.innerHTML = `Welcome ${account.name}`;
+    welcomeDiv.innerHTML = '';
     signInButton.classList.add('d-none');
     signOutButton.classList.remove('d-none');
 }
